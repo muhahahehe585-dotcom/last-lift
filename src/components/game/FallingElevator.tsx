b473291eaf0f24c16f7c8dbc1c7a774ae00f3e3f
Ansar Amirgali <ansaramirgali@Mac.lan>
2026-08-19 14:40:08 +0500

    Randomize lobby elevator drops

--- src/components/game/FallingElevator.tsx
+++ src/components/game/FallingElevator.tsx
@@ -1,9 +1,22 @@
+import { useEffect, useState, type CSSProperties } from 'react';
 import fallingElevatorUrl from '../../assets/falling-elevator.png';
 
+function randomStart() {
+  return 8 + Math.random() * 72;
+}
+
 export function FallingElevator() {
+  const [startX, setStartX] = useState(randomStart);
+  const style = { '--elevator-x': `${startX}vw` } as CSSProperties;
+
+  useEffect(() => {
+    const timer = window.setInterval(() => setStartX(randomStart()), 5000);
+    return () => window.clearInterval(timer);
+  }, []);
+
   return (
     <div className="falling-elevator" aria-hidden="true">
-      <img src={fallingElevatorUrl} alt="" />
+      <img src={fallingElevatorUrl} alt="" style={style} />
     </div>
   );
 }
